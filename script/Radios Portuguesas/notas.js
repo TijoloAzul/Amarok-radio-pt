0c63ca923bf41e523571a6e3e7d0a33d152da9bc
zipizap <zipizap123@gmail.com>
2010-03-22 17:16:58 +0100

    added notas.js...

---
+++ script/Radios Portuguesas/notas.js
@@ -0,0 +1,23 @@
+Station.
+  .name
++  .image
+  .url
+  .description
+
+  
++Category.
+  .name
+  image
+  .stationsList
+  
+  
+categories.
+  ."categ1-name"[]
+    [x] = station
+  ."categ2-name"[]
+    [x] = station
+    
+images.
+  ."categ1-name" = "categ1-image.gif"
+  ."categ2-name" = "categ2-image.gif"
+  
